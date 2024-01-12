a79c5a45c43e1329b0a403f6abe60c6719188963
Laurin Stapf <72888948+Laurino108@users.noreply.github.com>
2024-01-12 14:22:26 +0100

    feat(Components): adds button component (#1)
    * Publishing v0.1 of Button Component
    
    * Adjusted Button Component
    
    + Added ripple effect for android
    + Added automatic width which is ajusted by the width of the device
    
    * Updated Button Component on requirements from pull request

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Vibration, View, Platform } from 'react-native';
 import { AppSplashScreen } from './screens/loadingScreens/SplashScreen/AppSplashScreen';
+import * as Haptics from 'expo-haptics';
 import React, { useState } from 'react';
+import Button from './components/Button';
 
 export default function App() {
   const [isLoadingComplete, setLoadingComplete] = useState(false);
@@ -16,6 +18,13 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button
+        style={{ margin:30 }}
+        title="This is a test"
+        iconName="ArrowClockwise20Regular"
+        design="primary"
+        onPress={() => alert("Button pressed")}
+        onLongPress={() => {Platform.OS == 'android' ? Vibration.vibrate(1 * 40) : Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)} } />
     </View>
   );
 }
--- components/Button.jsx
+++ components/Button.jsx
@@ -1,49 +1,116 @@
 import React from 'react';
-import { TouchableOpacity, Text, Pressable } from 'react-native';
-import { Add12Regular } from "@fluentui/react-native-icons";
+import { Text, StyleSheet, Pressable, View, Dimensions, Vibration, Platform } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome'; // Importieren Sie Ihre bevorzugte Icon-Bibliothek
+import * as Icons from '@fluentui/react-native-icons';
+import * as Haptics from 'expo-haptics';
+import {TouchableRipple} from 'react-native-paper'
 
-const Button = ({ style, color, icon, size, text, onPress }) => {
-  let buttonStyle;
-  let textStyle;
-  let iconStyle;
+const deviceWidth = Dimensions.get('window').width;
 
-  // Set button style
-  if (style === 'filled') {
-    buttonStyle = {
-      backgroundColor: color,
-      padding: 10,
-      borderRadius: 5,
-    };
-    textStyle = {
-      color: 'white',
-      fontSize: size,
-    };
-  } else if (style === 'outline') {
-    buttonStyle = {
-      borderColor: color,
-      borderWidth: 1,
-      padding: 10,
-      borderRadius: 5,
-    };
-    textStyle = {
-      color: color,
-      fontSize: size,
-    };
-  }
+export default function Button(props) {
+  const {
+    onPress,
+    onLongPress,
+    title = 'Save',
+    size = 'medium',
+    design = 'primary',
+    iconName = '',  // Icon-Name als String
+    style = {},
+  } = props;
 
-  // Set icon style
-  if (icon) {
-    iconStyle = {
-      marginRight: 5,
-    };
-  }
+  // Dynamische Stilfunktionen
+  const getButtonStyle = () => {
+    let buttonWidth = deviceWidth - 80; // Subtract 20 units of margin from each side
+    switch (size) {
+      case 'large':
+        return { ...styles.big, width: buttonWidth };
+      case 'medium':
+      default:
+        return { ...styles.medium, width: buttonWidth };
+    }
+  };
+
+  const getTextColor = () => {
+    switch (design) {
+      case 'secondary':
+        return '#1868F1';
+      case 'tertiary':
+        return '#1868F1';
+      default:
+        return 'white';
+    }
+  };
+
+  const getDesignStyle = () => {
+    switch (design) {
+      case 'secondary':
+        return styles.outline;
+      case 'tertiary':
+        return styles.textOnlyDesign;
+      case 'primary':
+      default:
+        return {};
+    }
+  };
+
+  // Icon-Rendering-Funktion
+  const renderIcon = () => {
+    if (!iconName) return null;
+    
+    const IconComponent = Icons[iconName]; // Zugriff auf das spezifische Icon
+    if (!IconComponent) return null; // Falls kein Icon gefunden wird
+
+    const iconColor = getTextColor();
+    return (
+      <IconComponent size={20} color={iconColor} style={styles.icon} />
+    );
+  };
+
+  const textStyle = {
+    ...styles.text,
+    color: getTextColor(),
+    marginLeft: iconName ? 8 : 0,
+  };
 
   return (
-    <Pressable style={buttonStyle} onPress={onPress}>
-      {icon && <Ionicons name={icon} size={size} color={color} style={iconStyle} />}
-      <Text style={textStyle}>{text}</Text>
-    </Pressable>
+      <TouchableRipple borderless={true} style={[ styles.button, getButtonStyle(), getDesignStyle(), style]} rippleColor="rgba(255, 255, 255, .32)" onPress={onPress} onLongPress={onLongPress}>
+        <>
+          {renderIcon()}
+          <Text style={textStyle}>{title}</Text>
+        </>
+      </TouchableRipple>
   );
-};
+}
 
-export default Button;
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    display: 'flex',
+    borderRadius: 8,
+    padding: 8,
+  },
+  medium: {
+    height: 40,
+    backgroundColor: '#1868F1',
+  },
+  big: {
+    height: 52,
+    backgroundColor: '#1868F1',
+  },
+  text: {
+    fontSize: 13,
+  },
+  icon: {
+    gap: 8, // Abstand zwischen Icon und Text
+  },
+  outline: {
+    backgroundColor: 'transparent',
+    borderWidth: 1,
+    borderColor: '#1868F1',
+  },
+  textOnlyDesign: {
+    backgroundColor: 'transparent',
+  },
+});
