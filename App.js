7df662a8105d0c7e0d5e878687065876e4d14d42
Laurin Stapf <stapf_laurin@outlook.de>
2024-01-17 15:41:58 +0100

    Aligning the App.js to the main App.js

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, Vibration, View, Platform } from 'react-native';
-import { AppSplashScreen } from './screens/loadingScreens/SplashScreen/AppSplashScreen';
-import * as Haptics from 'expo-haptics';
-import React, { useState } from 'react';
-import Button from './components/Button';
-import Card from './components/Card/Card';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, Vibration, View, Platform } from "react-native";
+import { AppSplashScreen } from "./screens/loadingScreens/SplashScreen/AppSplashScreen";
+import * as Haptics from "expo-haptics";
+import React, { useState } from "react";
+import Button from "./components/Button";
+import StackNavigator from "./navigation/StackNavigator";
+import TestScreen from "./screens/TestScreen/TestScreen";
 
 export default function App() {
   const [isLoadingComplete, setLoadingComplete] = useState(false);
@@ -15,28 +16,14 @@ export default function App() {
     );
   }
 
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Button
-        style={{ margin: 10 }}
-        title="This is a test"
-        iconName="ArrowClockwise20Regular"
-        design="primary"
-        onPress={() => alert("Button pressed")}
-        onLongPress={() => {Platform.OS == 'android' ? Vibration.vibrate(1 * 40) : Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)} } />
-        <Card iconName="Server24Regular" title="Servername XYZ" description="192.178.68.1">
-        </Card>
-    </View>
-  );
+  return <StackNavigator />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
