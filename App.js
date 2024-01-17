705f8ad459bd584194fb8ee9374dfda5bf70c90a
Elias Sommer <sommer_elias@outlook.de>
2024-01-17 12:47:06 +0100

    adds stack navigator with template component/stack

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, Vibration, View, Platform } from 'react-native';
-import { AppSplashScreen } from './screens/loadingScreens/SplashScreen/AppSplashScreen';
-import * as Haptics from 'expo-haptics';
-import React, { useState } from 'react';
-import Button from './components/Button';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, Vibration, View, Platform } from "react-native";
+import { AppSplashScreen } from "./screens/loadingScreens/SplashScreen/AppSplashScreen";
+import * as Haptics from "expo-haptics";
+import React, { useState } from "react";
+import Button from "./components/Button";
+import StackNavigator from "./navigation/StackNavigator";
+import TestScreen from "./screens/TestScreen/TestScreen";
 
 export default function App() {
   const [isLoadingComplete, setLoadingComplete] = useState(false);
@@ -14,26 +16,14 @@ export default function App() {
     );
   }
 
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Button
-        style={{ margin:30 }}
-        title="This is a test"
-        iconName="ArrowClockwise20Regular"
-        design="primary"
-        onPress={() => alert("Button pressed")}
-        onLongPress={() => {Platform.OS == 'android' ? Vibration.vibrate(1 * 40) : Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)} } />
-    </View>
-  );
+  return <StackNavigator />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
