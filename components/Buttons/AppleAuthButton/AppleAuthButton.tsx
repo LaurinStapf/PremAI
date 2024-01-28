6ba72f8bd1cd68dbac48dbb8a2d523a0e8204fdf
Elias Sommer <sommer_elias@outlook.de>
2024-01-28 01:44:05 +0100

    Refactor button components and update styles

--- components/Buttons/AppleAuthButton/AppleAuthButton.tsx
+++ components/Buttons/AppleAuthButton/AppleAuthButton.tsx
@@ -1,42 +1,44 @@
-import React from 'react';
-import { TouchableOpacity, Image, Text, StyleSheet, View, Dimensions } from 'react-native';
+import React from "react";
+import {
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  Dimensions,
+} from "react-native";
 
-const deviceWidth = Dimensions.get('window').width;
+const deviceWidth = Dimensions.get("window").width;
 
 const AppleAuthButton = () => {
-    return (
-        <TouchableOpacity style={styles.container}>
-            <Text style={styles.icon}></Text>
+  return (
+    <TouchableOpacity style={styles.container}>
+      <Text style={styles.icon}></Text>
 
-            <Text style={styles.text}>
-                Continue with Apple
-            </Text>
-        </TouchableOpacity>
-    );
-}
+      <Text style={styles.text}>Continue with Apple</Text>
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#000000',
-        borderRadius: 8,
-        paddingVertical: 12,
-        paddingHorizontal: 16,
-        width: deviceWidth - 68,
-        
-    },
-    icon: {
-        fontSize: 20,
-        color: '#FFFFFF',
-        marginRight: 8
-    },
-    text: {
-        color: '#FFFFFF',
-        fontWeight: 'bold',
-        fontSize: 16,
-    },
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#000000",
+    borderRadius: 8,
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+    width: deviceWidth - 68,
+  },
+  icon: {
+    fontSize: 20,
+    color: "#FFFFFF",
+    marginRight: 8,
+  },
+  text: {
+    color: "#FFFFFF",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
 });
 
 export default AppleAuthButton;
--- screens/SignInScreen/SignInScreen.tsx
+++ screens/SignInScreen/SignInScreen.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { View, TextInput, StyleSheet, Text,  } from "react-native";
+import { View, TextInput, StyleSheet, Text, Platform, Dimensions,  } from "react-native";
 import Button from "../../components/Button";
 
 import AccountAvatar from "../../assets/account-avatar.svg";
-//import { Divider } from "react-native-paper";
 import AppleAuthButton from "../../components/Buttons/AppleAuthButton/AppleAuthButton";
 import GoogleAuthButton from "../../components/Buttons/GoogleAuthButton/GoogleAuthButton";
 
-// create a divider component with inline styles!
+import PremAiLogo from "../../assets/TextLogo.svg";
+
+const screenHeight = Dimensions.get('screen').height;
+
 const Divider = (props: any) => {
   return (
     <View {...props}>
@@ -41,18 +43,18 @@ const SignInScreen: React.FC = () => {
 
       <GoogleAuthButton />
 
+      <PremAiLogo style={{top: (Platform.OS === 'ios') ? screenHeight * 0.22 : screenHeight * 0.30}}/>
+
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    //centre the content
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
-  //style the text input make it wider and do something like fluent does from styles
   textInput: {
     width: "80%",
     padding: 15,
